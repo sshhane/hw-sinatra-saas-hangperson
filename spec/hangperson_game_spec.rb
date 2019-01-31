acfe0668c1f60647cea288a3d9f42f2f81ab5fb3
sshhane <g00332592@gmit.ie>
2019-01-31 14:34:43 +0000

    nine passed

--- spec/hangperson_game_spec.rb
+++ spec/hangperson_game_spec.rb
@@ -63,7 +63,7 @@ describe HangpersonGame do
         expect(@game.guess('a')).to be false
         expect(@game.guess('q')).to be false
       end
-      it 'is case insensitive', :pending => true do
+      it 'is case insensitive' do
         expect(@game.guess('A')).to be false
         expect(@game.guess('Q')).to be false
         expect(@game.guesses).not_to include('A')
